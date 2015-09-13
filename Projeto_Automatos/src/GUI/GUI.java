d5638ef35f7fec3ec99737c827ad434275a35fc3
Leonardo <lpbaiser@gmail.com>
2015-09-13 16:33:27 -0300

    Iniciando Interface

---
+++ Projeto_Automatos/src/GUI/GUI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author leonardo
+ */
+public class GUI extends JFrame{
+    
+    JButton btnAbrir = new JButton("Abrir");
+    
+    
+    public GUI(){
+        
+    }
+    
+}
