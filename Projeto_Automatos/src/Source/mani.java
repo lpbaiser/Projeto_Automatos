883e1d6f7b5cf29fa9ba221cc6477c774d6dfd97
emanuel <emanuel@Mazzer>
2015-09-14 20:00:20 -0300

    Manipulação

--- Projeto_Automatos/src/Source/mani.java
+++ Projeto_Automatos/src/Source/mani.java
@@ -23,8 +23,10 @@ public class mani {
     }
     
    public Automatos manipula(List<String> arq){
+       
        int aux=0;
        Transicao t=new Transicao();
+       t.estado1=new String[100][3];
        //String[] nova=arq.split("\n");
        Automatos aut=new Automatos();
        aut.setAlfabeto(arq.get(0).split(" "));
@@ -32,16 +34,22 @@ public class mani {
        aut.setIniciais(arq.get(2));
        aut.setFinais(arq.get(3).split(" "));
        int i=4;
+       System.out.println(arq.size());
        while(i<arq.size()){
-           t.estado1[aux]=(arq.get(i).split(" "));
+           String [] s=arq.get(i).split(" ");
+//           t.estado1[aux][0]=s[0];
+//           t.estado1[aux][1]=s[1];
+//           t.estado1[aux][2]=s[2];
+           t.estado1[aux]=s;
            //t.setEstado1()[i]; Setters em matrizes?
+           System.out.println(arq.get(i));
            i++;
            aux++;
        }
        aut.setTransicao(t);
        return aut;
    }
-   
+   ///data/BCC/4 Periodo/Automatos/Exemplo.txt
    public String[] getTransicoes(String trans){
        String[] transicao= trans.split("-");
        return transicao;
@@ -59,6 +67,8 @@ public class mani {
                }
                
            }
+           System.out.println(t.estado1[i][2]);
+           //System.out.println(aut.getIniciais());
            i++;
        }
        i=0;
@@ -67,6 +77,7 @@ public class mani {
                 return true;
             }
            
+            i++;
        }
        return false;
    }
