56b9ee09ef47b16e1e7799d8a95136b9bef65648
emanuel <emanuel@Mazzer>
2015-09-14 23:38:35 -0300

    Manipulação

--- Projeto_Automatos/src/Source/mani.java
+++ Projeto_Automatos/src/Source/mani.java
@@ -28,8 +28,6 @@ public class mani {
        int aux=0;
        Transicao t=new Transicao();
        t.estado1=new ArrayList<>();
-       //t.estado1;
-       //String[] nova=arq.split("\n");
        Automatos aut=new Automatos();
        aut.setAlfabeto(arq.get(0).split(" "));
        aut.setEstados(arq.get(1).split(" "));
@@ -48,7 +46,6 @@ public class mani {
        aut.setTransicao(t);
        return aut;
    }
-   ///data/BCC/4 Periodo/Automatos/Exemplo.txt
    public String[] getTransicoes(String trans){
        String[] transicao= trans.split("-");
   
@@ -59,31 +56,23 @@ public class mani {
        int aux=0;
        int i=0;
        String[] transicao=getTransicoes(transicoes);
-     //  System.out.println("tttttttttttttt"+transicoes.length());
-       while(aux<transicao.length){
-         //  System.out.println("aaaaaaaa"+t.estado1.get(i).get(1));
-           //System.out.println("bbbbbbbbb"+transicao[aux]);
+       while(aux<transicao.length&&i<t.estado1.size()){
            if(aut.getIniciais().equals(t.estado1.get(i).get(0))){
                if(transicao[aux].equals(t.estado1.get(i).get(1))){
-                   //aut.setIniciais(t.estado1[i][2]);
-                   if(!(aut.getIniciais().equals(t.estado1.get(i).get(2)))){
-                       i++;
+                   if((aut.getIniciais().equals(t.estado1.get(i).get(2)))){
+                       i--;
                    }
                    aut.setIniciais(t.estado1.get(i).get(2));
                    aux++;
-                   System.out.println("auxxxx"+aux);
                }
-               
            }
-           
-           //i++;
+           i++;
        }
        i=0;
        while(i<aut.getFinais().length){
             if (aut.getIniciais().equals(aut.getFinais()[i])){
                 return true;
             }
-           
             i++;
        }
        return false;
